526dded2b0eb368ab5ead00efdbf8b8fe037bcc6
cyberplanner <saleh.gfx@gmail.com>
2016-07-02 17:10:38 +0100

    Fixed example #2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,6 +3,9 @@ require 'fizzbuzz'
 describe 'FizzBuzz' do
   it 'plays FizzBuzz' do
     expect(fizzbuzz(2)).to eq 2
+  end
+  
+  it "plays FizzBuzz" do
     expect(fizzbuzz(3)).to eq "Fizz"
   end
 end
