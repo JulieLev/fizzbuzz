b097e64427fa4faf406f1c9f03a01e1a85d79fee
cyberplanner <saleh.gfx@gmail.com>
2016-07-02 21:04:13 +0100

    Final fizzbuzz refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,14 @@
 def fizzbuzz(n)
+  (n % 3 == 0 && n % 5 == 0) ? 'fizzbuzz' : n % 3 == 0 ? 'fizz' : n % 5 == 0 ? 'buzz' : n
+=begin
+    return "fizzbuzz"
+  elsif n % 3 == 0
+    return "fizz"
+  elsif n % 5 == 0
+    return "buzz" 
+  else
+    n
+  end
+=end
 
-if n % 3 == 0 && n % 5 == 0
-  return "fizzbuzz"
-elsif n % 3 == 0
-  return "fizz"
-elsif n % 5 == 0
-  return "buzz"
-else
-  n
-end
 end
